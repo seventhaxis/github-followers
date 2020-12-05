9bdb5013c0f5d98fbc8d966deb4e24636ceac714
Matt Brown <matt@seventhaxis.com>
2020-12-05 08:55:12 -0500

    adding GFButton and GFTextField classes

---
+++ Followers/Classes/GFButton.swift
@@ -0,0 +1,39 @@
+//
+//  GFButton.swift
+//  Followers
+//
+//  Created by Matt Brown on 12/5/20.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    private enum ViewMetrics {
+        static let textColor = UIColor.white
+        static let preferredFont = UIFont.preferredFont(forTextStyle: .headline)
+        
+        static let cornerRadius: CGFloat = 10.0
+    }
+
+    init(bgColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        setupView()
+        
+        backgroundColor = bgColor
+        setTitle(title, for: .normal)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel?.textColor = ViewMetrics.textColor
+        titleLabel?.font = ViewMetrics.preferredFont
+        
+        layer.cornerRadius = ViewMetrics.cornerRadius
+    }
+}
---
+++ Followers/Classes/GFTextField.swift
@@ -0,0 +1,68 @@
+//
+//  GFTextField.swift
+//  Followers
+//
+//  Created by Matt Brown on 12/5/20.
+//
+
+import UIKit
+
+enum GFTextFieldType {
+    case userSearch
+    
+    var placeholderText: String {
+        switch self {
+        case .userSearch:
+            return "Enter Username"
+        }
+    }
+}
+
+final class GFTextField: UITextField {
+    
+    private enum ViewMetrics {
+        static let backgroundColor = UIColor.tertiarySystemBackground
+        
+        static let textColor = UIColor.label
+        static let tintColor = UIColor.label
+        static let textAlignment = NSTextAlignment.center
+        static let font = UIFont.preferredFont(forTextStyle: .title2)
+        static let minimumFontSize: CGFloat = 12.0
+        
+        static let cornerRadius: CGFloat = 10.0
+        static let borderWidth: CGFloat = 2.0
+        static let borderColor = UIColor.systemGray4.cgColor
+    }
+    
+    private let type: GFTextFieldType
+    
+    init(type: GFTextFieldType) {
+        self.type = type
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupView() {
+        translatesAutoresizingMaskIntoConstraints = false
+        placeholder = type.placeholderText
+        autocorrectionType = .no
+        
+        backgroundColor = ViewMetrics.backgroundColor
+        
+        textColor = ViewMetrics.textColor
+        tintColor = ViewMetrics.tintColor
+        textAlignment = ViewMetrics.textAlignment
+        font = ViewMetrics.font
+        
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = ViewMetrics.minimumFontSize
+        
+        layer.cornerRadius = ViewMetrics.cornerRadius
+        layer.borderWidth = ViewMetrics.borderWidth
+        layer.borderColor = ViewMetrics.borderColor
+    }
+}
